04243c21854dadd23742535cb3e980f91912f824
DizzyMoon <mikkel.simons1@gmail.com>
2022-09-20 07:57:13 -0100

    Added paralax (very fancy)

--- src/main/java/kea/madspild/api/HomeController.java
+++ src/main/java/kea/madspild/api/HomeController.java
@@ -3,6 +3,7 @@ package kea.madspild.api;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class HomeController {
     @GetMapping("/")
